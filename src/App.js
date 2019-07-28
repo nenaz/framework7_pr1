1c6598ad34758f96782dd93f2a6fcc9ad2826f7a
mrnenaz <mrnenaz@gmail.com>
2019-07-28 22:56:19 +0300

    start framework7 on develop branch

--- src/App.js
+++ src/App.js
@@ -1,26 +1,31 @@
 import React from 'react';
 import logo from './logo.svg';
+import { App, Statusbar, View, Page, Navbar, Toolbar, Link } from 'framework7-react';
+import '../node_modules/framework7/css/framework7.bundle.min.css';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+export default () => (
+  // Main Framework7 App component where we pass Framework7 params
+  <App params={{ theme: 'auto', name: 'My App', id: 'com.demoapp.test' }}>
 
-export default App;
+    {/* Status bar overlay for full screen mode (Cordova or PhoneGap) */}
+    <Statusbar></Statusbar>
+
+    {/* Your main view, should have "main" prop */}
+    <View main>
+      {/*  Initial Page */}
+      <Page>
+        {/* Top Navbar */}
+        <Navbar title="Awesome App"></Navbar>
+        {/* Toolbar */}
+        <Toolbar bottom>
+          <Link>Link 1</Link>
+          <Link>Link 2</Link>
+        </Toolbar>
+        {/* Page Content */}
+        <p>Page content goes here</p>
+        <Link href="/about/">About App</Link>
+      </Page>
+    </View>
+  </App>
+)
--- src/index.js
+++ src/index.js
@@ -2,11 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
+// import * as serviceWorker from './serviceWorker';
+import Framework7 from 'framework7/framework7.esm.bundle.js';
+import Framework7React from 'framework7-react';
+
+Framework7.use(Framework7React);
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+// serviceWorker.unregister();
